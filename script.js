9289e4fa84b654e894e168ca2386c2aab5198e31
Jmanni2 <jmanni557@gmail.com>
2024-05-15 17:00:55 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Your head content here -->
+</head>
+<body>
+    <h1>Welcome to Make Money Website</h1>
+    <!-- Your website content here -->
+
+    <!-- Link to your JavaScript file -->
+    <script src="script.js"></script>
+</body>
+</html>
+
